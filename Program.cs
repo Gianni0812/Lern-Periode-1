d24b5a914b3ecf7339d307d7b5cd14e7068d377c
Gianni0812 <g.caruso.inf24@stud.bbbaden.ch>
2024-09-20 10:55:28 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,146 +1,116 @@
 ﻿using System;
 
-namespace Taschenrechner
+namespace Rechner
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine("Wie wollen Sie genannt werden?");
             string name = Console.ReadLine();
-            Console.WriteLine("Willkommen " + name);
-            bool isContinue = true;
-            while (isContinue)
+            Console.WriteLine($"Wilkommen {name}");
+
+            bool wiederholen = true;
+            double ergebnis = 0;
+
+            while (wiederholen)
             {
+                Console.ForegroundColor = ConsoleColor.Cyan; 
+                Console.WriteLine("Geben Sie bitte Ihre Rechnung ein (z.B. 12 + 5):");
+                string eingabe = Console.ReadLine();
 
-                double resultat = 0;
-                double Zahl1 = 0;
-                double Zahl2 = 0;
+                double zahl1 = 0, zahl2 = 0;
+                char operatorSymbol = ' ';
+                bool gefunden = false;
 
-                bool wiederholung2 = false;
-                while (!wiederholung2)
+                foreach (char c in eingabe)
                 {
-                    Console.WriteLine("Gebene Sie die erste Zahl ein");
-                    Console.ForegroundColor = ConsoleColor.Blue;
-                    string eingabe = Console.ReadLine();
-                    Console.ResetColor();   
-
-                    if (double.TryParse(eingabe, out Zahl1))
+                    if (c == '+' || c == '-' || c == '*' || c == '/')
                     {
-
-                        wiederholung2 = true;
-                    }
-                    else
-                    {
-                        Console.ForegroundColor= ConsoleColor.Red;
-                        Console.WriteLine("Geben Sie bitte ein Zahl ein! ");
-                        Console.ResetColor();
-
-                        wiederholung2 = false;
-
+                        operatorSymbol = c;
+                        string[] teile = eingabe.Split(c);
+                        if (teile.Length == 2 && double.TryParse(teile[0], out zahl1) && double.TryParse(teile[1], out zahl2))
+                        {
+                            gefunden = true;
+                            break;
+                        }
                     }
                 }
 
-          
-                bool wiederholung3 = false;
-                while (!wiederholung3)
+                if (!gefunden)
                 {
-                    Console.WriteLine("Geben Sie die zweite Zahl ein");
-                    Console.ForegroundColor = ConsoleColor.Blue;
-                    string eingabe2 = Console.ReadLine();
-                    Console.ResetColor();
-
-                    if (double.TryParse(eingabe2, out Zahl2))
-                    {
-
-                        wiederholung3 = true;
-                    }
-                    else
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Geben Sie bitte ein Zahl ein! ");
-                        Console.ResetColor();
-                        wiederholung3 = false;
-
-                    }
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Ungültige Eingabe. Bitte versuchen Sie es erneut.");
+                    continue; 
                 }
+
+                switch (operatorSymbol)
+                {
+                    case '+':
+                        ergebnis = zahl1 + zahl2;
+                        break;
+                    case '*':
+                        ergebnis = zahl1 * zahl2;
+                        break;
+                    case '-':
+                        ergebnis = zahl1 - zahl2;
+                        break;
+                    case '/':
+                        if (zahl2 == 0)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red; 
+                            Console.WriteLine("Fehler: Division durch 0 ist nicht erlaubt.");
+                        }
+                        else
+                        {
+                            ergebnis = zahl1 / zahl2;
+                        }
+                        break;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Red; 
+                        Console.WriteLine("Ungültiger Operator. Bitte versuchen Sie es erneut.");
+                        break;
+                }
+
                 
-                
-                bool wiederholung1 = false;
-                while (!wiederholung1)
+                if (operatorSymbol != '/' || zahl2 != 0)
                 {
-                    Console.WriteLine(name + ", wie wollen Sie rechnen? (+,-,*,/) ");
-                    Console.ForegroundColor = ConsoleColor.Blue;
-                    string rechnung = Console.ReadLine();
-                    Console.ResetColor();
-
-
-                    if (rechnung == "+")
-                    {
-                        resultat = Zahl1 + Zahl2;
-                        wiederholung1 = true;
-                    }
-                    else if (rechnung == "-")
-                    {
-                        resultat = Zahl1 - Zahl2;
-                        wiederholung1 = true;
-                    }
-                    else if (rechnung == "*")
-                    {
-                        resultat = Zahl1 * Zahl2;
-                        wiederholung1 = true;
-                    }
-                    else if (rechnung == "/")
-                    {
-                        resultat = Zahl1 / Zahl2;
-                        wiederholung1 = true;
-                    }
-                    else
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Geben Sie bitte eine der gennanten texte ein (+,-,*,/) ");
-                        Console.ResetColor();
-                        wiederholung1 = false;
-                    }
-
+                    Console.ForegroundColor = ConsoleColor.Green; 
+                    Console.WriteLine($"{name}, Ihr Ergebnis lautet: {ergebnis}");
                 }
-                Console.WriteLine("Ihr Resultat lautet:");
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine(resultat);
-                Console.ResetColor();
 
-                bool wiederholung = false;
-                while (!wiederholung)
+                
+                bool ungültigeAntwort = true;
+                while (ungültigeAntwort)
                 {
-                    Console.WriteLine(name + ", wollen Sie noch eine Rechnung eingeben? Schreiben Sie bitte Ja oder Nein.");
+                    Console.ForegroundColor = ConsoleColor.Cyan; 
+                    Console.WriteLine($"{name}, wollen Sie noch eine Rechnung eingeben? (Ja oder Nein)");
                     string weiter = Console.ReadLine();
 
                     if (string.Equals(weiter, "nein", StringComparison.OrdinalIgnoreCase))
                     {
-                        isContinue = false;
-                        wiederholung = true;
+                        wiederholen = false;
+                        ungültigeAntwort = false; 
                     }
                     else if (string.Equals(weiter, "ja", StringComparison.OrdinalIgnoreCase))
                     {
-                        isContinue = true;
-                        wiederholung = true;
+                        ungültigeAntwort = false; 
                     }
                     else
                     {
                         Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Sie müssen Ja oder Nein schreiben.");
-                        Console.ResetColor();
-                        wiederholung = false;
+                        Console.WriteLine("Sie müssen 'Ja' oder 'Nein' schreiben.");
                     }
-                    
                 }
-                              
-         
             }
-            Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.WriteLine("Okey " + name + ", ich wünsche Ihnen noch einen schönen Tag.");
+
+            
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Vielen Dank für die Nutzung des Rechners!");
+
+            
             Console.ResetColor();
         }
-
     }
 }
